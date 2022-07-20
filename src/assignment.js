279fa5f389743b585f0a4539d1014944019bbe1d
Iredola <abdulrazaqraheem224@gmail.com>
2022-07-20 21:38:09 +0000

    attempt-1

--- src/assignment.js
+++ src/assignment.js
@@ -13,12 +13,11 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    
-    return arrayOfNumbers.length;
-}
- assignment.sumOfNumbers = sumOfNumbers;
-
+ var array = [1, 2, 3, 4, 5];
+ var sum = array.reduce(function(a, b){
+     return a + b;
+ }, 0);
+ console.log(sum); // Prints: 15
 /**
  * Challenge - 2
  * 
